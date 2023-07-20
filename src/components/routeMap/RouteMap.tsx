149a319e5b703a0203cfbc731bda0b8cadf42420
IlyaGoncharovY <goncharov.i@rambler.ru>
2023-07-20 18:03:37 +0300

    feat(render table and map) added new components RouteMap.tsx and TableForCoordinates.tsx

---
+++ src/components/routeMap/RouteMap.tsx
@@ -0,0 +1,51 @@
+import React from 'react';
+import { MapContainer, Marker, Polyline, TileLayer } from 'react-leaflet';
+import {LatLngExpression, LatLngTuple} from 'leaflet';
+import {RouteData} from "../table/TableForCoordinates";
+
+interface IRouteMap {
+    selectedRoute: RouteData | null
+}
+
+export const RouteMap: React.FC<IRouteMap> = ({ selectedRoute }) => {
+    const defaultLatLng: LatLngTuple = [59.84660399, 30.29496392]
+    const zoom: number = 13
+
+    const routeCoords: LatLngTuple[] | null =
+        selectedRoute && selectedRoute.key
+            ? [
+                selectedRoute.point1.split(',').map((coord) => parseFloat(coord.trim())) as LatLngTuple,
+                selectedRoute.point2.split(',').map((coord) => parseFloat(coord.trim())) as LatLngTuple,
+                selectedRoute.point3.split(',').map((coord) => parseFloat(coord.trim())) as LatLngTuple,
+            ]
+            : null
+
+    const routeStyle = {
+        color: 'blue',
+        weight: 2,
+    };
+
+    return (
+        <div>
+            <MapContainer
+                id="mapId"
+                center={routeCoords ? routeCoords[0] : defaultLatLng}
+                zoom={routeCoords ? zoom : zoom}
+                style={{ height: '100vh', width: '100vw' }}
+            >
+                <TileLayer
+                    url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                    attribution="&copy; <a href=&quot;http://osm.org/copyright&quot;>OpenStreetMap</a> contributors"
+                />
+                {routeCoords && <Polyline positions={routeCoords} pathOptions={routeStyle} />}
+                {routeCoords &&
+                    routeCoords.map((coords, index) => (
+                        <Marker key={index} position={coords as LatLngExpression}>
+                           marker
+                        </Marker>
+                    ))}
+            </MapContainer>
+        </div>
+    );
+};
+
---
+++ src/components/table/TableForCoordinates.tsx
@@ -0,0 +1,83 @@
+import React, {FC} from 'react';
+import {Table} from 'antd';
+
+export type RouteData = {
+    key: string
+    name: string
+    point1: string
+    point2: string
+    point3: string
+}
+
+interface ITableForCoordinates {
+    onRouteSelection: (route: RouteData) => void
+    selectedRoute: RouteData | null
+}
+
+export const TableForCoordinates:FC<ITableForCoordinates> = ({onRouteSelection, selectedRoute}) => {
+
+    const columns = [
+        {
+            title: 'Маршрут',
+            dataIndex: 'name',
+        },
+        {
+            title: 'Точка 1',
+            dataIndex: 'point1',
+        },
+        {
+            title: 'Точка 2',
+            dataIndex: 'point2',
+        },
+        {
+            title: 'Точка 3',
+            dataIndex: 'point3',
+        },
+    ]
+
+    const dataSource: RouteData[] = [
+        // your route data
+        {
+            key: '1',
+            name: 'Маршрут №1',
+            point1: "59.84660399, 30.29496392",
+            point2: "59.82934196, 30.42423701",
+            point3: "59.83567701, 30.38064206",
+        },
+        {
+            key: '2',
+            name: 'Маршрут №2',
+            point1: "59.82934196, 30.42423701",
+            point2: "59.82761295, 30.41705607",
+            point3: "59.84660399, 30.29496392",
+        },
+        {
+            key: '3',
+            name: 'Маршрут №3',
+            point1: "59.83567701, 30.38064206",
+            point2: "59.84660399, 30.29496392",
+            point3: "59.82761295, 30.41705607",
+        },
+    ]
+
+    const handleRowClick = (record: RouteData) => {
+        onRouteSelection(record)
+    }
+
+    return (
+        <div>
+            <Table columns={columns}
+                   dataSource={dataSource}
+                   size="small"
+                   onRow={(record) => ({
+                       onClick: () => handleRowClick(record),
+                       style: {
+                           background: record.key === selectedRoute?.key ? 'lightblue' : 'white',
+                       },
+                   })}
+            />
+        </div>
+    )
+
+};
+
