c479de8f4040f9995a37207293f119d1ee705359
IlyaGoncharovY <goncharov.i@rambler.ru>
2023-07-27 19:13:50 +0300

    fix(RouteMap.tsx) use hook and refactor component

--- src/components/routeMap/RouteMap.tsx
+++ src/components/routeMap/RouteMap.tsx
@@ -1,53 +1,38 @@
-import React, {FC} from 'react';
-import {MapContainer, Marker, Polyline, TileLayer} from 'react-leaflet';
+import React from 'react';
 import {LatLngTuple} from 'leaflet';
+import {MapContainer, Marker, Polyline, Popup, TileLayer} from 'react-leaflet';
+
+import {useRouteMap} from "../../utils/hooks/useRouteMap";
 import {useAppSelector} from "../../store/config/hook/hook";
 
+import s from "./RouteMap.module.scss"
 
-export const RouteMap: FC = () => {
+const ZOOM_LEVEL = 10
+const ROUTE_STYLE = {color: 'blue', weight: 3}
+const CENTER_POSITION: LatLngTuple = [59.84660399, 30.29496392]
+
+export const RouteMap = () => {
 
     const selectedRoute = useAppSelector(state => state.routes.selectedRoute)
-    const routeStyle = {
-        color: 'blue',
-        weight: 3,
-    }
-
-    const routeCoordinates: LatLngTuple[] | null =
-        selectedRoute &&
-        selectedRoute.routes[0].geometry.coordinates.map((coord: number[]) => [coord[1], coord[0]])
-
-    // useEffect(() => {
-    //     console.log('Selected Route:', selectedRoute)
-    //     console.log('Route Coordinates:', routeCoordinates)
-    // }, [selectedRoute, routeCoordinates])
-
-    const initialPosition = selectedRoute && routeCoordinates && routeCoordinates[0]
-    const defaultLatLng: LatLngTuple = initialPosition || [59.84660399, 30.29496392]
-
-    const centerPosition: LatLngTuple = initialPosition || defaultLatLng
-    const zoomLevel = 13
+    const {routeCoordinates, selectedRouteData, points} = useRouteMap(selectedRoute)
 
     return (
         <div>
             <MapContainer
                 id="mapId"
-                center={centerPosition}
-                zoom={zoomLevel}
-                style={{ height: '100vh', width: '100vw' }}
-            >
+                center={CENTER_POSITION}
+                zoom={ZOOM_LEVEL}
+                className={s.container}>
                 <TileLayer
                     url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
                     attribution="&copy; <a href=&quot;http://osm.org/copyright&quot;>OpenStreetMap</a> contributors"
                 />
-
-                {routeCoordinates && <Polyline positions={routeCoordinates} {...routeStyle} />}
-
-                {selectedRoute &&
-                    selectedRoute.routes[0].legs[0].steps.map((step, index) => (
-                        <Marker
-                            key={index}
-                            position={[step.maneuver.location[1], step.maneuver.location[0]]}
-                        />
+                {routeCoordinates && <Polyline positions={routeCoordinates} {...ROUTE_STYLE} />}
+                {selectedRouteData &&
+                    points?.map((point, index) => (
+                        <Marker key={index} position={[point[0], point[1]]}>
+                            <Popup>{`Point ${index + 1}`}</Popup>
+                        </Marker>
                     ))}
             </MapContainer>
         </div>
