97c8a634cb504fae86ec17c3daa22f52a777a5f1
IlyaGoncharovY <goncharov.i@rambler.ru>
2023-07-27 19:15:46 +0300

    feat(portal) portal for error

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,24 +1,41 @@
-import React from 'react';
-import './App.css';
-import {TableForCoordinates} from "./table/TableForCoordinates";
-import {RouteMap} from "./routeMap/RouteMap";
+import React, {useRef} from 'react';
+import {createPortal} from "react-dom";
 import {Layout} from 'antd';
+
+import {RouteMap} from "./routeMap/RouteMap";
+import {useAppSelector} from "../store/config/hook/hook";
+import {ErrorMessage} from "../common/Components/ErrorMessage";
+import {TableForCoordinates} from "./table/TableForCoordinates";
+
+import s from "./App.module.scss";
 
 const {Content} = Layout;
 
 function App() {
 
+    const portalRef = useRef(document.getElementById('error-portal'));
+    const error = useAppSelector(state => state.error.error)
 
     return (
-        <div className="App">
-            <div className="table-column">
-                <TableForCoordinates />
+        <div className={s.appContainer}>
+            <div className={s.tableColumn}>
+                <TableForCoordinates/>
             </div>
-            <div className="map-column">
-                <Content style={{padding: '0 24px', minHeight: 280}}>
+            <div className={s.mapColumn}>
+                <Content className={s.mapContent}>
                     <RouteMap/>
                 </Content>
             </div>
+            {portalRef.current && createPortal(
+                error && (
+                    <div className="modalContainer">
+                        <div className="errorMessageContainer">
+                            <ErrorMessage error={error}/>
+                        </div>
+                    </div>
+                ),
+                portalRef.current
+            )}
         </div>
     );
 }
